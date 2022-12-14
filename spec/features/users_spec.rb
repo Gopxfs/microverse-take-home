2c9c34dfe2cd1e145ee7886deb7f5750aa35f99a
Gabriel <gopxfs@hotmail.com>
2022-12-14 04:39:01 -0300

    Add tests to users page

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -2,16 +2,48 @@ require 'rails_helper'
 
 RSpec.describe "Users actions", type: :feature do
   describe "/users" do
-
-  end
-
-  describe "/users/:id" do
     before do
-      visit '/users/1'
+      visit '/users'
     end
 
-    it "finds user name on the page" do
-      expect(page).to have_content("James")
+    it 'contains a button to load the content' do
+      expect(page).to have_button('Load users data')
+    end
+
+    it 'displays a table with data, after database is populated' do
+      User.create(
+        index: 1,
+        first_name: 'Gabriel',
+        last_name: 'Fonseca',
+        status: 'Active',
+        email: 'gabriel@email.com',
+        created_at: '2018-04-08'
+      )
+      User.create(
+        index: 2,
+        first_name: 'Another',
+        last_name: 'Person',
+        status: 'Active',
+        email: 'another@email.com',
+        created_at: '2021-06-01'
+      )
+      User.create(
+        index: 3,
+        first_name: 'Other',
+        last_name: 'One',
+        status: 'Inactive',
+        email: 'other@email.com',
+        created_at: '2015-10-11'
+      )
+      visit '/users'
+      expect(page).to have_content(/First name/i)
+      expect(page).to have_content(/Last name/i)
+      expect(page).to have_content(/Status/i)
+      expect(page).to have_content(/Email/i)
+      expect(page).to have_content(/Created at/i)
+      expect(page).to have_content(/Gabriel/i)
+      expect(page).to have_content(/another@email.com/i)
+      expect(page).to have_content(/Inactive/i)
     end
   end
 end
