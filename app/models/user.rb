be1a3924e075388812775c906088ee9d55a09bba
Gabriel <gopxfs@hotmail.com>
2022-12-13 23:15:51 -0300

    Add helper method to check for user id

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   validates :status, presence: true
   validates :email, presence: true, length: { minimum: 1, maximum: 100 }
   validates :created_at, presence:true
+
+  def self.check_id(id)
+    User.where(index: id).empty?
+  end
 end
